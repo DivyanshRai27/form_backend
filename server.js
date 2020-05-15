1cb7eff68c96b38e10a777babe321b34f0002303
DivyanshRai27 <divyanshrai27@gmail.com>
2020-05-16 01:23:42 +0530

    jquery

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 
+//chunk 1
 const app = express();
 const log = console.log;
+const path = require('path')
 
 const PORT = 8080
 
+//chunk2
+//Data parsing
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(express.json())
 
+app.post('/email', (req, res) => {
+    //TODO
+})
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
 
 app.listen(PORT, () => {
     log('Server is running on port 8080')
