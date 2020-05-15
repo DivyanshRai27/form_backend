7634a7da2ae3acafad62641966e4d37e6e7bb0bc
DivyanshRai27 <divyanshrai27@gmail.com>
2020-05-16 03:42:40 +0530

    TEsting

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/public', express.static(path.join(__dirname, 'public/')))
 
 
 app.get('/', (req, res) => {
-    res.render('contact',  {layout: false})
+    res.render('contact', {layout: false})
 })
 
 app.post('/send', (req, res) => {
@@ -30,12 +30,48 @@ app.post('/send', (req, res) => {
     <h3>Contact Details</h3>
     <ul>
         <li>Name: ${req.body.name}</li>
-        <li>Name: ${req.body.company}</li>
-        <li>Name: ${req.body.email}</li>
-        <li>Name: ${req.body.phone}</li>
+        <li>Company: ${req.body.company}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Phone: ${req.body.phone}</li>
+        
     </ul>
+
+    <h3>Message</h3>
+    <p>Message: ${req.body.message}</p>
     
     `
+
+    // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+  tls:{
+    rejectUnuthorized: false
+  }
+
+  // send mail with defined transport object
+  let info = transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  req.render('sent', {layout: false})
 })
 
 app.listen(PORT, () => {
